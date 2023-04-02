e700dd3ea1ce36027f1b505dce84ab5ef2597dc9
Pavel Volkov <eternal.vvanderer8@gmail.com>
2023-04-02 19:05:33 +0300

    Added lab3

---
+++ lab3/ShootingRange/ShootingRange/ShootingRange.cs
@@ -0,0 +1,143 @@
+﻿public interface IShootingRangeObserver
+{
+    void windSpeedChanged();
+
+    void windDirectionChanged();
+
+    void projectileSpeedChanged();
+
+    void targetMoved();
+
+    void targetHit();
+
+    void shotMissed();
+}
+
+namespace ShootingRange
+{
+    public enum WindDirection
+    {
+        NORTH,
+        SOUTH,
+        WEST,
+        EAST
+    }
+
+    public class ShootingRangeObservable
+    {
+        private List<IShootingRangeObserver> subscribers = new List<IShootingRangeObserver>();
+        private uint distanceToTarget = 10;
+        private uint projectileSpeed = 10;
+        private uint windSpeed = 10;
+        private WindDirection windDirection = WindDirection.NORTH;
+
+        public void notifySubscribersWindSpeedChanged()
+        {
+            subscribers.ForEach(observer => observer.windSpeedChanged());
+        }
+
+        public void notifySubscribersTargetMoved()
+        {
+            subscribers.ForEach(observer => observer.targetMoved());
+        }
+
+        public void notifySubscribersProjectileSpeedChanged()
+        {
+            subscribers.ForEach(observer => observer.projectileSpeedChanged());
+        }
+
+        public void notifySubscribersTargetHit()
+        {
+            subscribers.ForEach(observer => observer.targetHit());
+        }
+
+        public void notifySubscribersShotMissed()
+        {
+            subscribers.ForEach(observer => observer.shotMissed());
+        }
+
+        public void notifySubscribersWindDirectionChanged()
+        {
+            subscribers.ForEach(observer => observer.windDirectionChanged());
+        }
+
+        public void setWindSpeed(uint windSpeed)
+        {
+            if (this.windSpeed!= windSpeed)
+            {
+                this.windSpeed = windSpeed;
+                notifySubscribersWindSpeedChanged();
+            }
+        }
+
+        public void setProjectileSpeed(uint projectileSpeed)
+        {
+            if (this.projectileSpeed != projectileSpeed)
+            {
+                this.projectileSpeed = projectileSpeed;
+                notifySubscribersProjectileSpeedChanged();
+            }
+        }
+
+        public void setDistanceToTarget(uint distanceToTarget)
+        {
+            if (this.distanceToTarget != distanceToTarget)
+            {
+                this.distanceToTarget = distanceToTarget;
+                notifySubscribersTargetMoved();
+            }
+        }
+
+        public void setWindDirection(WindDirection windDirection)
+        {
+            if (this.windDirection != windDirection)
+            {
+                this.windDirection = windDirection;
+                notifySubscribersWindDirectionChanged();
+            }
+        }
+
+        public Int32 subscribe(IShootingRangeObserver observer)
+        {
+            subscribers.Add(observer);
+
+            return subscribers.Count - 1;
+        }
+
+        public void unsubscribe(Int32 index)
+        {
+            subscribers.RemoveAt(index);
+        }
+
+        public void shoot(long correction)
+        {
+            uint flyingTime = distanceToTarget / projectileSpeed;
+
+            if (windDirection == WindDirection.NORTH || windDirection == WindDirection.SOUTH)
+            {
+                if (correction == 0)
+                {
+                    notifySubscribersTargetHit();
+                }
+                else
+                {
+                    notifySubscribersShotMissed();
+                }
+                return;
+            }
+
+            long directWindSpeed = windDirection == WindDirection.WEST
+                ? -windSpeed
+                : windSpeed;
+
+            if (directWindSpeed * flyingTime == correction)
+            {
+                notifySubscribersTargetHit();
+            }
+            else
+            {
+                notifySubscribersShotMissed();
+            }
+        }
+    }
+}
---
+++ lab3/ShootingRange/ShootingRangeTests/ShootingRangeTests.cs
@@ -0,0 +1,341 @@
+﻿using ShootingRange;
+using Moq;
+
+namespace ShootingRangeTests
+{
+    public class ShootingRangeTests
+    {
+        ShootingRangeObservable shootingRange = new ShootingRangeObservable();
+
+        Mock<IShootingRangeObserver> observerMock = new Mock<IShootingRangeObserver>();
+
+        [Fact]
+        public void Change_wind_speed_observer_notified()
+        {
+            shootingRange.subscribe(observerMock.Object);
+
+            shootingRange.setWindSpeed(100);
+
+            observerMock.Verify(observer => observer.windSpeedChanged(), Times.Once());
+        }
+
+        [Fact]
+        public void Change_wind_speed_observer_not_notified()
+        {
+            shootingRange.subscribe(observerMock.Object);
+
+            shootingRange.setWindSpeed(10);
+
+            observerMock.Verify(observer => observer.windSpeedChanged(), Times.Never());
+        }
+
+        [Fact]
+        public void Change_wind_direction_observer_notified()
+        {
+            shootingRange.subscribe(observerMock.Object);
+
+            shootingRange.setWindDirection(WindDirection.EAST);
+
+            observerMock.Verify(observer => observer.windDirectionChanged(), Times.Once());
+        }
+
+        [Fact]
+        public void Change_wind_direction_observer_not_notified()
+        {
+            shootingRange.subscribe(observerMock.Object);
+
+            shootingRange.setWindDirection(WindDirection.NORTH);
+
+            observerMock.Verify(observer => observer.windDirectionChanged(), Times.Never());
+        }
+
+        [Fact]
+        public void Change_projectile_speed_observer_notified()
+        {
+            shootingRange.subscribe(observerMock.Object);
+
+            shootingRange.setProjectileSpeed(100);
+
+            observerMock.Verify(observer => observer.projectileSpeedChanged(), Times.Once());
+        }
+
+        [Fact]
+        public void Change_projectile_speed_observer_not_notified()
+        {
+            shootingRange.subscribe(observerMock.Object);
+
+            shootingRange.setProjectileSpeed(10);
+
+            observerMock.Verify(observer => observer.projectileSpeedChanged(), Times.Never());
+        }
+
+        [Fact]
+        public void Change_target_move_observer_notified()
+        {
+            shootingRange.subscribe(observerMock.Object);
+
+            shootingRange.setDistanceToTarget(100);
+
+            observerMock.Verify(observer => observer.targetMoved(), Times.Once());
+        }
+
+        [Fact]
+        public void Change_target_move_observer_not_notified()
+        {
+            shootingRange.subscribe(observerMock.Object);
+
+            shootingRange.setDistanceToTarget(10);
+
+            observerMock.Verify(observer => observer.targetMoved(), Times.Never());
+        }
+
+        [Fact]
+        public void Shoot_with_north_wind_direction_target_hit_observer_notified()
+        {
+            shootingRange.subscribe(observerMock.Object);
+
+            shootingRange.shoot(0);
+
+            observerMock.Verify(observer => observer.targetHit(), Times.Once());
+            observerMock.Verify(observer => observer.shotMissed(), Times.Never());
+        }
+
+        [Fact]
+        public void Shoot_with_south_wind_direction_target_hit_observer_notified()
+        {
+            shootingRange.subscribe(observerMock.Object);
+
+            shootingRange.setWindDirection(WindDirection.SOUTH);
+            shootingRange.shoot(0);
+
+            observerMock.Verify(observer => observer.targetHit(), Times.Once());
+            observerMock.Verify(observer => observer.shotMissed(), Times.Never());
+        }
+
+        [Fact]
+        public void Shoot_with_south_wind_direction_shot_missed_observer_notified()
+        {
+            shootingRange.subscribe(observerMock.Object);
+
+            shootingRange.setWindDirection(WindDirection.SOUTH);
+            shootingRange.shoot(10);
+
+            observerMock.Verify(observer => observer.targetHit(), Times.Never());
+            observerMock.Verify(observer => observer.shotMissed(), Times.Once());
+        }
+
+        [Fact]
+        public void Shoot_with_north_wind_direction_shot_missed_observer_notified()
+        {
+            shootingRange.subscribe(observerMock.Object);
+
+            shootingRange.setWindDirection(WindDirection.SOUTH);
+            shootingRange.shoot(-10);
+
+            observerMock.Verify(observer => observer.targetHit(), Times.Never());
+            observerMock.Verify(observer => observer.shotMissed(), Times.Once());
+        }
+
+        [Fact]
+        public void Shoot_with_east_wind_direction_target_hit_observer_notified()
+        {
+            shootingRange.subscribe(observerMock.Object);
+
+            shootingRange.setWindDirection(WindDirection.EAST);
+            shootingRange.shoot(10);
+
+            observerMock.Verify(observer => observer.targetHit(), Times.Once());
+            observerMock.Verify(observer => observer.shotMissed(), Times.Never());
+        }
+
+        [Fact]
+        public void Shoot_with_east_wind_direction_shot_missed_observer_notified()
+        {
+            shootingRange.subscribe(observerMock.Object);
+
+            shootingRange.setWindDirection(WindDirection.EAST);
+            shootingRange.shoot(100);
+
+            observerMock.Verify(observer => observer.targetHit(), Times.Never());
+            observerMock.Verify(observer => observer.shotMissed(), Times.Once());
+        }
+
+        [Fact]
+        public void Shoot_with_west_wind_direction_target_hit_observer_notified()
+        {
+            shootingRange.subscribe(observerMock.Object);
+
+            shootingRange.setWindDirection(WindDirection.WEST);
+            shootingRange.shoot(-10);
+
+            observerMock.Verify(observer => observer.targetHit(), Times.Once());
+            observerMock.Verify(observer => observer.shotMissed(), Times.Never());
+        }
+
+        [Fact]
+        public void Shoot_with_west_wind_direction_shot_missed_observer_notified()
+        {
+            shootingRange.subscribe(observerMock.Object);
+
+            shootingRange.setWindDirection(WindDirection.WEST);
+            shootingRange.shoot(100);
+
+            observerMock.Verify(observer => observer.targetHit(), Times.Never());
+            observerMock.Verify(observer => observer.shotMissed(), Times.Once());
+        }
+
+        [Fact]
+        public void Change_wind_speed_unsubscribed_observer_not_notified()
+        {
+            var index = shootingRange.subscribe(observerMock.Object);
+            shootingRange.setWindSpeed(100);
+            shootingRange.unsubscribe(index);
+            shootingRange.setWindSpeed(120);
+
+            observerMock.Verify(observer => observer.windSpeedChanged(), Times.Once());
+        }
+
+        [Fact]
+        public void Change_wind_direction_unsubscribed_observer_not_notified()
+        {
+            var index = shootingRange.subscribe(observerMock.Object);
+            shootingRange.setWindDirection(WindDirection.EAST);
+            shootingRange.unsubscribe(index);
+            shootingRange.setWindDirection(WindDirection.SOUTH);
+
+            observerMock.Verify(observer => observer.windDirectionChanged(), Times.Once());
+        }
+
+        [Fact]
+        public void Change_projectile_speed_unsubscribed_observer_not_notified()
+        {
+            var index = shootingRange.subscribe(observerMock.Object);
+            shootingRange.setProjectileSpeed(100);
+            shootingRange.unsubscribe(index);
+            shootingRange.setProjectileSpeed(120);
+
+            observerMock.Verify(observer => observer.projectileSpeedChanged(), Times.Once());
+        }
+
+        [Fact]
+        public void Change_target_moved_unsubscribed_observer_not_notified()
+        {
+            var index = shootingRange.subscribe(observerMock.Object);
+            shootingRange.setDistanceToTarget(100);
+            shootingRange.unsubscribe(index);
+            shootingRange.setDistanceToTarget(120);
+
+            observerMock.Verify(observer => observer.targetMoved(), Times.Once());
+        }
+
+        [Fact]
+        public void Shoot_with_north_wind_direction_target_hit_unsubscribed_observer_not_notified()
+        {
+            var index = shootingRange.subscribe(observerMock.Object);
+
+            shootingRange.shoot(0);
+            shootingRange.unsubscribe(index);
+            shootingRange.shoot(0);
+
+            observerMock.Verify(observer => observer.targetHit(), Times.Once());
+            observerMock.Verify(observer => observer.shotMissed(), Times.Never());
+        }
+
+        [Fact]
+        public void Shoot_with_north_wind_direction_shot_missed_unsubscribed_observer_not_notified()
+        {
+            var index = shootingRange.subscribe(observerMock.Object);
+
+            shootingRange.shoot(10);
+            shootingRange.unsubscribe(index);
+            shootingRange.shoot(10);
+
+            observerMock.Verify(observer => observer.targetHit(), Times.Never());
+            observerMock.Verify(observer => observer.shotMissed(), Times.Once());
+        }
+
+        [Fact]
+        public void Shoot_with_south_wind_direction_target_hit_unsubscribed_observer_not_notified()
+        {
+            var index = shootingRange.subscribe(observerMock.Object);
+
+            shootingRange.setWindDirection(WindDirection.SOUTH);
+            shootingRange.shoot(0);
+            shootingRange.unsubscribe(index);
+            shootingRange.shoot(0);
+
+            observerMock.Verify(observer => observer.targetHit(), Times.Once());
+            observerMock.Verify(observer => observer.shotMissed(), Times.Never());
+        }
+
+        [Fact]
+        public void Shoot_with_south_wind_direction_shot_missed_unsubscribed_observer_not_notified()
+        {
+            var index = shootingRange.subscribe(observerMock.Object);
+
+            shootingRange.setWindDirection(WindDirection.SOUTH);
+            shootingRange.shoot(10);
+            shootingRange.unsubscribe(index);
+            shootingRange.shoot(10);
+
+            observerMock.Verify(observer => observer.targetHit(), Times.Never());
+            observerMock.Verify(observer => observer.shotMissed(), Times.Once());
+        }
+
+        [Fact]
+        public void Shoot_with_east_wind_direction_shot_missed_unsubscribed_observer_not_notified()
+        {
+            var index = shootingRange.subscribe(observerMock.Object);
+
+            shootingRange.setWindDirection(WindDirection.EAST);
+            shootingRange.shoot(100);
+            shootingRange.unsubscribe(index);
+            shootingRange.shoot(100);
+
+            observerMock.Verify(observer => observer.targetHit(), Times.Never());
+            observerMock.Verify(observer => observer.shotMissed(), Times.Once());
+        }
+
+        [Fact]
+        public void Shoot_with_east_wind_direction_target_hit_unsubscribed_observer_not_notified()
+        {
+            var index = shootingRange.subscribe(observerMock.Object);
+
+            shootingRange.setWindDirection(WindDirection.EAST);
+            shootingRange.shoot(10);
+            shootingRange.unsubscribe(index);
+            shootingRange.shoot(10);
+
+            observerMock.Verify(observer => observer.targetHit(), Times.Once());
+            observerMock.Verify(observer => observer.shotMissed(), Times.Never());
+        }
+
+        [Fact]
+        public void Shoot_with_west_wind_direction_target_hit_unsubscribed_observer_not_notified()
+        {
+            var index = shootingRange.subscribe(observerMock.Object);
+
+            shootingRange.setWindDirection(WindDirection.WEST);
+            shootingRange.shoot(-10);
+            shootingRange.unsubscribe(index);
+            shootingRange.shoot(-10);
+
+            observerMock.Verify(observer => observer.targetHit(), Times.Once());
+            observerMock.Verify(observer => observer.shotMissed(), Times.Never());
+        }
+
+        [Fact]
+        public void Shoot_with_west_wind_direction_shot_missed_unsubscribed_observer_not_notified()
+        {
+            var index = shootingRange.subscribe(observerMock.Object);
+
+            shootingRange.setWindDirection(WindDirection.WEST);
+            shootingRange.shoot(100);
+            shootingRange.unsubscribe(index);
+            shootingRange.shoot(100);
+
+            observerMock.Verify(observer => observer.targetHit(), Times.Never());
+            observerMock.Verify(observer => observer.shotMissed(), Times.Once());
+        }
+    }
+}
